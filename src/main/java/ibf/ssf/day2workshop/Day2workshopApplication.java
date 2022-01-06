779b9692cc8d501ff579bc43c32abdcbd6d53cac
Firdaus <mohdfirdaus94@outlook.com>
2022-01-06 11:33:27 +0800

    Initial commit

---
+++ src/main/java/ibf/ssf/day2workshop/Day2workshopApplication.java
@@ -0,0 +1,13 @@
+package ibf.ssf.day2workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2workshopApplication.class, args);
+	}
+
+}
